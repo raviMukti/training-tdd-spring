f4b934d94a46b3679401242a6cd12e963d5d9b63
raviMukti <shinjiravi@gmail.com>
2021-10-13 20:42:11 +0700

    Inisialisasi

---
+++ src/main/java/training/tdd/spring/TrainingTddSpringApplication.java
@@ -0,0 +1,13 @@
+package training.tdd.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingTddSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingTddSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/training/tdd/spring/TrainingTddSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package training.tdd.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingTddSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
